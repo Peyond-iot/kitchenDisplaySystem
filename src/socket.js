6ee68930d8413691d1f03ae46ad5f82606ebdb9d
Peyond-iot <peyondiotsolutions@gmail.com>
2025-01-04 19:13:05 +0900

    updated backend link over localhost

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // Initialize the socket connection
-const socket = io("http://localhost:5000"); // Replace with your actual backend URL
+const socket = io("https://backend-nwcq.onrender.com"); // Replace with your actual backend URL
 
 // Export the socket instance
 export default socket;
